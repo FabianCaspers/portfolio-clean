7c6d1f9f5ce47cb7cb3593d2cfef1c9481423fe4
FabianCaspers <fabian.c1308@icloud.com>
2023-06-13 19:50:52 +0200

    responsive for iphone and ipad

--- portfolio-clean/src/app/components/navbar/navbar.component.ts
+++ portfolio-clean/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  @ViewChild('mobileMenuButton') mobileMenuButton!: ElementRef;
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
+
+  ngOnInit(): void {
+      
+  }
+
+  openAndCloseMenu(){
+    this.mobileMenuButton.nativeElement.classList.toggle('active');
+    this.mobileMenu.nativeElement.classList.toggle('show');
+  }
 
 }
